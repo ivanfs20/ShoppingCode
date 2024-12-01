c52789c76358635e96eb7818fdaebd6297fc4f90
Carlos Iván Flores Sánchez <carloscras12@outlook.com>
2024-12-01 15:45:49 -0600

    Inicio del proyecto

---
+++ src/main/java/com/ShoppingCode/ShoppingCodeApplication.java
@@ -0,0 +1,13 @@
+package com.ShoppingCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ShoppingCode/ShoppingCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShoppingCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
